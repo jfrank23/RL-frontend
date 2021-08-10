ab31ee2716bea6af5a4f851128a14e071e78b5ed
Jordan Franklin <jordo189@gmail.com>
2021-08-10 14:41:43 -0400

    refactor

--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@material-ui/core";
-import { Fragment } from "react";
 import Example from "./examplePass";
 
 const Home = () => (
   <div>
     <h1>Home</h1>
-    <Button variant="contained" color="primary">I am a button</Button>
+    <Button variant="contained" color="primary">
+      I am a button
+    </Button>
     <Example input1="jordan"></Example>
   </div>
 );
 
-export default Home
+export default Home;
