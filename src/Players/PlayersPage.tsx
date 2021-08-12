3d1a685b0902682c1bb0f6739a2a02b8b1bc16be
Hoa Lu <luthanhhoa2009@gmail.com>
2021-08-13 06:49:49 +0700

    player page

--- src/Players/PlayersPage.tsx
+++ src/Players/PlayersPage.tsx
@@ -1,9 +1,35 @@
-import { Fragment } from "react";
+import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
+import { useEffect, useState } from "react";
+import { Player } from "../common/models/Player";
+import PlayerService from "../common/Services/PlayerService";
+import TeamService from "../common/Services/TeamService";
 
 const PlayersPage = () => (
-  <Fragment>
+  <div>
     <h1>Players Page</h1>
-  </Fragment>
+    <div style={{height:500, width:'100%'}}>
+      <DataGrid 
+        rows={rows}
+        columns={columns}
+        pageSize={10}
+      />
+    </div>
+  </div>
 );
+
+const rows  : GridRowsProp = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'XGrid', col2: 'is Awesome' },
+  { id: 3, col1: 'Material-UI', col2: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Player ID', width: 200 },
+  { field: 'col2', headerName: 'First Name', width: 200 },
+  { field: 'col3', headerName: 'Last Name', width: 200 },
+  { field: 'col4', headerName: 'Games Played', width: 200 },
+  { field: 'col5', headerName: 'Games Won', width: 200 },
+  { field: 'col6', headerName: 'Details', width: 200 },
+];
 
 export default PlayersPage;
