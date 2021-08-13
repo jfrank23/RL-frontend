f128b11751c2ccca336051ba94fa3ce0445bcdc8
Hoa Lu <luthanhhoa2009@gmail.com>
2021-08-13 16:20:57 +0700

    playersPage with mock data (need real data to implement and test services later)

--- src/Players/PlayersPage.tsx
+++ src/Players/PlayersPage.tsx
@@ -1,35 +1,50 @@
 import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
+import { TablePagination } from '@material-ui/core';
 import { useEffect, useState } from "react";
 import { Player } from "../common/models/Player";
 import PlayerService from "../common/Services/PlayerService";
 import TeamService from "../common/Services/TeamService";
 
-const PlayersPage = () => (
-  <div>
-    <h1>Players Page</h1>
-    <div style={{height:500, width:'100%'}}>
-      <DataGrid 
-        rows={rows}
-        columns={columns}
-        pageSize={10}
-      />
-    </div>
-  </div>
-);
+const players: Player[] = [
+  {id: 456, firstName: 'Hoa', lastName: 'Lu' }, 
+  {id: 123, firstName: 'Jordan', lastName: 'Franklin' },
+  {id: 273, firstName: 'Ming', lastName: 'Yang' },
+  {id: 1493, firstName: 'Joe', lastName: 'Franzo' },
+  {id: 812, firstName: 'Simon', lastName: 'Szymanski' },
+];
 
-const rows  : GridRowsProp = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'XGrid', col2: 'is Awesome' },
-  { id: 3, col1: 'Material-UI', col2: 'is Amazing' },
+const PlayersPage = () => {
+  return (
+    <div>
+      <h1>Players Page</h1>
+      <div style={{height: 400, width:'100%'}}>
+        <DataGrid 
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+        />
+      </div>
+    </div>
+  );
+};
+
+const rows : GridRowsProp = [
+  ... players.map((player) => ({
+    id: player.id, 
+    playerId: player.id, 
+    firstName: player.firstName, 
+    lastName: player.lastName,
+  }))
 ];
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Player ID', width: 200 },
-  { field: 'col2', headerName: 'First Name', width: 200 },
-  { field: 'col3', headerName: 'Last Name', width: 200 },
-  { field: 'col4', headerName: 'Games Played', width: 200 },
-  { field: 'col5', headerName: 'Games Won', width: 200 },
-  { field: 'col6', headerName: 'Details', width: 200 },
+  { field: 'playerId', headerName: 'Player ID', width: 200 },
+  { field: 'firstName', headerName: 'First Name', width: 200 },
+  { field: 'lastName', headerName: 'Last Name', width: 200 },
+  { field: 'gamesPlayed', headerName: 'Games Played', width: 200 },
+  { field: 'gamesWon', headerName: 'Games Won', width: 200 },
+  { field: 'details', headerName: 'Details', width: 200 },
 ];
 
 export default PlayersPage;
