ca74bf4585571668e0011660c66fa506268648b2
simon <simon.c.szymanski@gmail.com>
2021-08-14 05:36:29 -0400

    initial card implementation with static data

--- src/Games/GamesPage.tsx
+++ src/Games/GamesPage.tsx
@@ -1,9 +1,38 @@
 import { Fragment } from "react";
+import Card from "./Card";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "20px",
+    paddingRight: "20px",
+  },
+});
+
+//const classes = useStyles();
 
 const Games = () => (
   <Fragment>
     <h1>Games Page</h1>
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card />
+      </Grid>
+    </Grid>
   </Fragment>
 );
 
-export default Games
+export default Games;
