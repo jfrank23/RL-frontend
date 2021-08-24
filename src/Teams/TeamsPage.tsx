ecfd374b2a4c4b3aab7ff16b10cad754f1816495
Hoa Lu <luthanhhoa2009@gmail.com>
2021-08-25 05:55:13 +0700

    adjust PlayersPage + TeamsPage

--- src/Teams/TeamsPage.tsx
+++ src/Teams/TeamsPage.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import { useEffect, useState } from "react";
-import IconButton from '@material-ui/core/IconButton';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,6 +10,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import './TeamsPage.css'
@@ -27,7 +27,7 @@ import TeamService from "../common/Services/TeamService";
 const useRowStyles = makeStyles({
   root: {
     '& > *': {
-      borderBottom: 'unset',
+      borderBottom: 'set',
     },
   },
 });
@@ -49,6 +49,16 @@ const ranks: Rank[] = [
   {id: 542, rank: 435, teamId: 791, gameId: 981123},
   {id: 712, rank: 932, teamId: 912, gameId: 985121},
   {id: 823, rank: 521, teamId: 821, gameId: 981512},
+  {id: 2461, rank: 246, teamId: 8211, gameId: 981510},
+  {id: 4681, rank: 468, teamId: 8212, gameId: 981511},
+  {id: 4671, rank: 467, teamId: 8213, gameId: 981513},
+  {id: 9861, rank: 986, teamId: 8214, gameId: 981514},
+  {id: 5171, rank: 517, teamId: 8215, gameId: 981515},
+  {id: 8182, rank: 812, teamId: 8216, gameId: 981516},
+  {id: 6811, rank: 681, teamId: 8217, gameId: 981517},
+  {id: 8721, rank: 872, teamId: 8218, gameId: 981518},
+  {id: 5871, rank: 587, teamId: 8219, gameId: 981519},
+  {id: 7131, rank: 713, teamId: 8220, gameId: 981531},
 ];
 
 const players: Player[] = [
@@ -80,7 +90,6 @@ const game1 : Stat[] = [
 ];
 
 const currentTime : Date = new Date();
-console.log(currentTime);
 const games: Game[] = [
   {id: 956511, blueTeam: {id: 123, team: players1}, redTeam: {id: 912, team: players2}, gameTime: currentTime, redScore: 40, blueScore: 20, stats: game1},
 ];
@@ -136,10 +145,11 @@ function Row(props: {row: any}) {
 const Teams = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, ranks.length - page * rowsPerPage);
   const handleChangePage = (event:any, newPage:any) => {
     setPage(newPage);
   };
-  const handleChangeRowsPerPage = (event:any) => {
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
@@ -166,9 +176,17 @@ const Teams = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {ranks.map((rankTeam) => (
+            {(rowsPerPage > 0
+             ? ranks.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) 
+             : ranks)
+             .map((rankTeam:any) => (
               <Row key={rankTeam.rank} row={rankTeam} />
             ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
