a13c8f87f13ac729b292024eb5582aebe6ec6a1f
mingy7601 <35078148+mingy7601@users.noreply.github.com>
2021-08-31 11:24:51 -0400

    team gen layout

--- src/Teams/TeamGenStyles.tsx
+++ src/Teams/TeamGenStyles.tsx
@@ -2,11 +2,6 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core";
 
 export const teamGenStyles = makeStyles((theme: Theme) =>
   createStyles({
-    paper: {
-      marginLeft: "5rem",
-      marginRight: "5rem",
-      padding: "5rem",
-    },
     title:{
       marginTop: theme.spacing(1),
       marginBottom: theme.spacing(1),
@@ -40,6 +35,33 @@ export const teamGenStyles = makeStyles((theme: Theme) =>
           theme.palette.type === "light" ? "#f0f0f0" : "#303030"
         }`,
       },
+    },
+    grid: {
+      flexGrow: 1,
+    },
+    paper_right: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      marginLeft: "1rem",
+      marginRight: "5rem",
+    },
+    paper_left: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      marginLeft: "5rem",
+      marginRight: "1rem",
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+      marginLeft: "5rem",
+      marginRight: "5rem",
+    },
+    pageName: {
+      marginLeft: "5rem",
     },
   })
 );
--- src/Teams/TeamGenerator.tsx
+++ src/Teams/TeamGenerator.tsx
@@ -1,33 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Player } from "../common/models/Player";
 import {
     Paper,
     Typography,
     Button,
 } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
 
-import {
-    withStyles,
-    Theme,
-    createStyles,
-    makeStyles,
-  } from "@material-ui/core/styles";
-import { DataGrid, GridRowsProp, GridColDef } from '@material-ui/data-grid';
+import { DataGrid, GridRowsProp, GridColDef, GridSelectionApi, GridSelectionModel } from '@material-ui/data-grid';
 import { teamGenStyles } from "./TeamGenStyles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TablePagination from "@material-ui/core/TablePagination";
-import IconButton from "@material-ui/core/IconButton";
-import SearchIcon from "@material-ui/icons/Search";
+import ReactDOM from "react-dom";
 
+function NumberList(props: any){
+    const n = props.teams;
+    const listItems = n.map((n:any) =>
+        <li key={n.toString()}>
+            {n}
+        </li>
+);
+    return (
+        <ul> {listItems} </ul>
+    );
+}
 
 const TeamGenerator = () => {
     const [pageSize, setPageSize] = React.useState<number>(10);
-    const players: Player[] = [
+    let players: Player[] = [
         {id: 456, firstName: 'A', lastName: 'Lu' }, 
         {id: 123, firstName: 'B', lastName: 'Franklin' },
         {id: 273, firstName: 'C', lastName: 'Yang' },
@@ -66,14 +64,25 @@ const TeamGenerator = () => {
           },
     ];
 
+    let selectedPlayerIDs:any[] = [];
+    let playyyerrrz:any[] = [];
+    let team1:string[] = [];
+    let team2:string[] = [];
 
-    var team1:string[] = [];
-    var team2:string[] = [];
-    var num_players = players.length;
-
+    let num_players = players.length;
     const classes = teamGenStyles();
+    function handleCheckbox(e:GridSelectionModel){
+        selectedPlayerIDs = e;
+        console.log(selectedPlayerIDs);
+        playyyerrrz = [];
+        for(let i = 0; i < e.length; i++){
+            
+        }
+    }
 
     const handleGenerate = () => {
+        team1 = [];
+        team2 = [];
         if(num_players > 8){
             num_players = 8;
         }
@@ -94,69 +103,70 @@ const TeamGenerator = () => {
                     team2.push(players[i].firstName);
                 }
             }
+            ReactDOM.render(
+                <NumberList teams={team2} />,
+                document.getElementById('team2')
+            );
+            ReactDOM.render(
+                <NumberList teams={team1} />,
+                document.getElementById('team1')
+            );
         }
-        return [team1, team2];
+
     };
-    if(num_players > 8){
-        num_players = 8;
-    }
-    for (let i = 0; i < players.length; i++){
-        if(Math.random() < 0.5){
-            if(team1.length === Math.floor(num_players/2)){
-                team2.push(players[i].firstName);
-            }else {
-                team1.push(players[i].firstName);
-            }
-        } else {
-            if(team2.length === Math.floor(num_players/2)){
-                team1.push(players[i].firstName);
-            }else {
-                team2.push(players[i].firstName);
-            }
-        }
-    }
+
     return (
         <div>
-            <Typography variant="h3" className={classes.title}>Generate a Team</Typography>
-            <Paper className={classes.paper}>
-                <div style={{ height: "100%", width: "100%" }} className={classes.root}>
-                <DataGrid
-                    checkboxSelection
-                    rows={rows}
-                    columns={columns}
-                    pageSize={pageSize}
-                    onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                    rowsPerPageOptions={[5, 10, 25]}
-                    disableColumnMenu
-                    autoHeight={true}
-                    pagination
-                    onSelectionModelChange={e => console.log(e)}
-                />
-                </div>
-
-                <Typography variant="h6" className={classes.title}>{ num_players } players selected</Typography>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleGenerate}
-                >
-                    Generate
-                </Button>
-            </Paper>
-            
-            <Paper className={classes.paper}>
-                <h1>{ Math.floor(num_players/2) } V { Math.floor(num_players/2) }</h1>
-                <Paper className={classes.teams}>
-                    <Typography variant="h6" className={classes.title}>Team 1</Typography>
-                    <Typography variant="h6" className={classes.title}>{ team1 }</Typography>
-                </Paper>
-                <Paper className={classes.teams}>
-                    <Typography variant="h6" className={classes.title}>Team 2</Typography>
-                    <Typography variant="h6" className={classes.title} >{ team2 }</Typography>
-                </Paper>
-            </Paper>
-
-            
+            <Typography variant="h3" className={classes.pageName}>Generate a Team</Typography>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper_left}>
+                        <div style={{ height: "100%", width: "100%" }} className={classes.root}>
+                            <DataGrid
+                                checkboxSelection
+                                rows={rows}
+                                columns={columns}
+                                pageSize={pageSize}
+                                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                                rowsPerPageOptions={[5, 10, 25]}
+                                disableColumnMenu
+                                autoHeight={true}
+                                pagination
+                                onSelectionModelChange={ (e) => handleCheckbox(e) }
+                                
+                            />
+                        </div>
+                        <Typography variant="h6" className={classes.title}>{ num_players } players selected</Typography>
+                        <Typography variant="h6" className={classes.title}> { /*selectedPlayerIDs*/ }</Typography>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={ handleGenerate }
+                        >
+                            Generate
+                        </Button>
+                    </Paper> 
+                </Grid>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper_right}>
+                        <h1>{ Math.floor(num_players/2) } V { Math.floor(num_players/2) }</h1>
+                        <Grid container spacing={3}>
+                            <Grid item xs={6}>
+                                <Paper className={classes.teams}>
+                                    <Typography variant="h6"  className={classes.title}>Team 1</Typography>
+                                    <Typography  variant="h6" id="team1" className={classes.title}></Typography>
+                                </Paper>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Paper className={classes.teams}>
+                                    <Typography  variant="h6" className={classes.title}>Team 2</Typography>
+                                    <Typography  variant="h6" id="team2" className={classes.title}></Typography>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
         </div>
 
     );
